a35383e159696ad56ab98d1310d15887c70764a0
sweetdough <khhrogers@gmail.com>
2024-02-07 04:38:11 -0500

    my server is up and running

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// once a request is received send this to the page 
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
 });
 
+// to listen for web request 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
